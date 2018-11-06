67380e6af537231f12f5a73cdedac2f0d23b51d8
Laure Leter <laure.leter@gmail.com>
2018-11-06 13:52:34 +0100

    prepared for heroku

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def competence
+  end
+
+  def lawyers
+  end
+
+  def news
+  end
+
+  def contact
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  root to: 'pages#home'
+  get 'competence', to: 'pages#competence', as: :competence
+  get 'lawyers', to: 'pages#lawyers', as: :lawyers
+  get 'news', to: 'pages#news', as: :news
+   get 'contact', to: 'pages#contact', as: :contact
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
