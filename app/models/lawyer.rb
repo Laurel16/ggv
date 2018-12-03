815e6981d1d1ef5fd945e43c683ef11b0b9c6a39
Laure Leter <laure.leter@gmail.com>
2018-12-03 22:28:38 +0100

    again

--- app/models/lawyer.rb
+++ app/models/lawyer.rb
@@ -1,5 +1,5 @@
 class Lawyer < ApplicationRecord
 COMPETENCES = [:"labour", :"corporate", :"commercial", :"real_estate", :"taxes", :"compliance", :"litigation" ]
-translates :first_name, :last_name, :competences, :competences_details, :presentation, :languages, :bar, :course, :vCard, :pdf, :status, :competences_2, :competences_3, :mail, :phone, :address
+translates :first_name, :last_name, :competences, :competences_details, :presentation, :languages, :bar, :course, :vCard, :pdf, :status, :competences_2, :competences_3, :mail, :phone, :address, :illustation, :illustration_cache
   mount_uploader :picture, PhotoUploader
 end
