1cf6fa9fca69b19b66348ba3bba6dd11f7831730
Laure Leter <laure.leter@gmail.com>
2018-12-03 15:37:04 +0100

    pb to push heroku with globalize

--- app/models/lawyer.rb
+++ app/models/lawyer.rb
@@ -1,5 +1,5 @@
 class Lawyer < ApplicationRecord
 COMPETENCES = [:"labour", :"corporate", :"commercial", :"real_estate", :"taxes", :"compliance", :"litigation" ]
-translates :competences, :competences_details, :presentation, :languages, :bar, :course, :vCard, :pdf, :status, :competences_2, :competences_3
+translates :first_name, :last_name, :competences, :competences_details, :presentation, :languages, :bar, :course, :vCard, :pdf, :status, :competences_2, :competences_3, :picture, :picture_cache, :email, :phone, :adress
   mount_uploader :picture, PhotoUploader
 end
