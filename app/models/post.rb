50b41343bae702ffad17c78336de6a4485c2d7de
Laure Leter <laure.leter@gmail.com>
2018-12-03 15:46:30 +0100

    again

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
 
   SUBJECTS = [:"event", :"conference", :"media"]
-  translates :title, :content, :category, :date, :place
+  translates :title, :content, :category, :date, :place, :illustration, :illustration_cache
   mount_uploader :illustration, PhotoUploader
 end
